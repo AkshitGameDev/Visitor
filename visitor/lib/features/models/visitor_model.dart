0c8124de28137ef4ead8c903de0afaf3f79d2000
Akshit Sharma <akshitsharma196@gmail.com>
2026-05-08 12:57:06 -0400

    updating backend logic

--- visitor/lib/features/models/visitor_model.dart
+++ visitor/lib/features/models/visitor_model.dart
@@ -4,12 +4,22 @@ class Visitor {
   final String email;
   final String phone;
 
+  final String purpose;
+  final DateTime entryTime;
+  final DateTime? exitTime;
+
   Visitor({
     required this.id,
     required this.name,
     required this.email,
     required this.phone,
+
+    required this.purpose,
+    required this.entryTime,
+    this.exitTime,
   });
+
+  bool get isPunchedOut => exitTime != null;
 
   factory Visitor.fromJson(Map<String, dynamic> json) {
     return Visitor(
@@ -17,6 +27,15 @@ class Visitor {
       name: json["name"] ?? "",
       email: json["email"] ?? "",
       phone: json["phone"] ?? "",
+
+      // NEW
+      purpose: json["purpose"] ?? "",
+      entryTime: DateTime.parse(
+        json["entryTime"] ?? DateTime.now().toIso8601String(),
+      ),
+      exitTime: json["exitTime"] != null
+          ? DateTime.parse(json["exitTime"])
+          : null,
     );
   }
 
@@ -25,6 +44,11 @@ class Visitor {
       "name": name,
       "email": email,
       "phone": phone,
+
+      // NEW
+      "purpose": purpose,
+      "entryTime": entryTime.toIso8601String(),
+      "exitTime": exitTime?.toIso8601String(),
     };
   }
 }
